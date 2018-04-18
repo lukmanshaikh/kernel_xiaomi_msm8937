dfdef630aa5d06f20b995c68ed32ad1839f5d461
nichcream <c.potato23@gmail.com>
2018-04-18 16:52:06 +0800

    wake_gestures: Return failure at dev_filter by default

--- drivers/input/touchscreen/wake_gestures.c
+++ drivers/input/touchscreen/wake_gestures.c
@@ -504,10 +504,8 @@ static int input_dev_filter(struct input_dev *dev) {
 	} else if (strstr(dev->name, "ist30xx_ts_input")) {
 		hw_version = IMAGIS;
 		return 0;
-	} else {
-		return 1;
 	}
-	return 0;
+	return 1;
 }
 
 static int wg_input_connect(struct input_handler *handler,
